3caad2c6c17125f3700ac24e7f97332e871c6763
hamzakhalem <hamzakhalem97@gmail.com>
2024-08-20 17:56:16 +0100

    new version

--- main/users/serializers.py
+++ main/users/serializers.py
@@ -1,13 +1,18 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
-
+from .models import Profile
+class ProfileSerializers(serializers.ModelSerializer):
+    user = serializers.HyperlinkedRelatedField(read_only= True, many=False, view_name='user-detail')
+    class Meta:
+        model= Profile
+        fields =['url', 'id', 'user', 'image']
 
 class UserSerializers(serializers.ModelSerializer):
     
     password = serializers.CharField(write_only=True, required=False)
     username = serializers.CharField(read_only=True)
     old_password = serializers.CharField(write_only=True, required= False)
+    profile = ProfileSerializers(read_only=True)
 
     def validate(self, data):
         reqmeth = self.context['request'].method
@@ -46,4 +51,4 @@ class UserSerializers(serializers.ModelSerializer):
         return super(UserSerializers, self).update(instance, validated_data)
     class  Meta:
         model = User
-        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name', 'password', 'old_password']
+        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name', 'password', 'old_password', 'profile']
--- main/users/viewsets.py
+++ main/users/viewsets.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User
-
+from .models import Profile
 from rest_framework import viewsets
-from .serializers import UserSerializers
+from .serializers import UserSerializers, ProfileSerializers
 from .permissions import IsUserOwnerOrGetAndPostOnly
 
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [IsUserOwnerOrGetAndPostOnly,]
     queryset = User.objects.all()
     serializer_class = UserSerializers
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializers
