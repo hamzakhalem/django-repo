a8619837e08e9c2285c5c10cf67d8cb8e7b3ee8e
hamzakhalem <hamzakhalem97@gmail.com>
2024-08-21 10:05:46 +0100

    new version

--- main/users/permissions.py
+++ main/users/permissions.py
@@ -13,3 +13,16 @@ class IsUserOwnerOrGetAndPostOnly(permissions.BasePermission):
             return request.user == obj
         
         return False
+class IsProfileOwnerOrGetAndPostOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return True
+    
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        if not request.user.is_anonymous:
+            return request.user.profile == obj
+        
+        return False
--- main/users/viewsets.py
+++ main/users/viewsets.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.models import User
 from .models import Profile
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from .serializers import UserSerializers, ProfileSerializers
-from .permissions import IsUserOwnerOrGetAndPostOnly
+from .permissions import IsUserOwnerOrGetAndPostOnly, IsProfileOwnerOrGetAndPostOnly
 
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [IsUserOwnerOrGetAndPostOnly,]
     queryset = User.objects.all()
     serializer_class = UserSerializers
-class ProfileViewSet(viewsets.ModelViewSet):
+class ProfileViewSet(viewsets.GenericViewSet, mixins.RetrieveModelMixin, mixins.UpdateModelMixin):
+    permission_classes = [IsProfileOwnerOrGetAndPostOnly,]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializers
