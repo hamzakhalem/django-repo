38f7bb04486c84b86268074dd5451db6dab3f8ec
hamzakhalem <hamzakhalem97@gmail.com>
2024-09-07 20:30:29 +0100

    task app

--- main/task/permissions.py
+++ main/task/permissions.py
@@ -10,19 +10,6 @@ class IsTaskListOwnerOrGetAndPostOnly(permissions.BasePermission):
             return True
         
         if not request.user.is_anonymous:
-            return True
-        
-        return False
-
-    def has_permission(self, request, view):
-        return True
-    
-    def has_object_permission(self, request, view, obj):
-
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        
-        if not request.user.is_anonymous:
             return request.user.profile == obj.created_by
-        
+
         return False
