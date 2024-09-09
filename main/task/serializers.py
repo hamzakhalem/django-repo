2ff3e69a91bb404317e572bc4f30638c19be3c40
hamzakhalem <hamzakhalem97@gmail.com>
2024-09-09 13:37:29 +0100

    task serialzers

--- main/task/serializers.py
+++ main/task/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import TaskList
+from .models import TaskList, Attachment, Task
 from house.models import House
 
 class TaskListSerializers(serializers.ModelSerializer):
@@ -9,3 +9,25 @@ class TaskListSerializers(serializers.ModelSerializer):
         model= TaskList
         fields =['url', 'id', 'name', 'created_on', 'created_by', 'house', 'description', 'status'  ]
         read_only_fields = ['created_on', 'status', 'notcompleted_tasks_count']
+
+
+class TaskSerializers(serializers.ModelSerializer):
+
+    created_by = serializers.HyperlinkedRelatedField(read_only=True, many=False, view_name = 'profile-detail')
+    completed_by = serializers.HyperlinkedRelatedField(read_only=True, many=False, view_name = 'profile-detail')
+    task_list = serializers.HyperlinkedRelatedField(queryset= TaskList.objects.all(), many=False, view_name = 'tasklist-detail')
+    class Meta:
+        model= Task
+        fields =['url', 'id', 'name', 'created_on', 'completed_on', 'created_by', 'completed_by',
+                 'description', 'status', 'task_lis'  ]
+        read_only_fields = ['created_on', 'completed_on', 'created_by', 'completed_by',
+                            'status', 'notcompleted_tasks_count']
+
+
+class AttachmentSerializers(serializers.ModelSerializer):
+    
+    task_list = serializers.HyperlinkedRelatedField(queryset= Task.objects.all(), many=False, view_name = 'task-detail')
+    class Meta:
+        model= Task
+        fields =['url', 'id', 'data', 'created_on', 'task']
+        read_only_fields = ['created_on']
