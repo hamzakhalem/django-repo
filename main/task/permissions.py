fc615d92290b693aaffb04bb31ff436640b3663f
hamzakhalem <hamzakhalem97@gmail.com>
2024-09-10 15:22:26 +0100

    task serialzers

--- main/task/permissions.py
+++ main/task/permissions.py
@@ -13,3 +13,29 @@ class IsTaskListOwnerOrGetAndPostOnly(permissions.BasePermission):
             return request.user.profile == obj.created_by
 
         return False
+
+class IsTaskOwnerOrGetAndPostOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_anonymous:
+            return request.user.house != None
+        return False
+    
+    def has_object_permission(self, request, view, obj):
+
+        if not request.user.is_anonymous:
+            return request.user.profile == obj.task_list.house
+
+        return False
+
+class IsAttachmentOwnerOrGetAndPostOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if not request.user.is_anonymous:
+            return request.user.house != None
+        return False
+    
+    def has_object_permission(self, request, view, obj):
+
+        if not request.user.is_anonymous:
+            return request.user.profile == obj.task.task_list.house
+
+        return False
--- main/task/viewsets.py
+++ main/task/viewsets.py
@@ -3,8 +3,8 @@ from rest_framework import viewsets, status, mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from .serializers import TaskListSerializers
-from .permissions import IsTaskListOwnerOrGetAndPostOnly
+from .serializers import TaskListSerializers,TaskSerializers, AttachmentSerializers
+from .permissions import IsTaskListOwnerOrGetAndPostOnly, IsTaskOwnerOrGetAndPostOnly
 
 from django.contrib.auth.models import User
 
@@ -13,3 +13,15 @@ class TaskListViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins
     permission_classes = [IsTaskListOwnerOrGetAndPostOnly,]
     queryset = TaskList.objects.all()
     serializer_class = TaskListSerializers
+
+class TasktViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsTaskOwnerOrGetAndPostOnly,]
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializers
+    
+class AttachmenttViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
+             mixins.UpdateDestroyMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+    permission_classes = [IsAttachmentOwnerOrGetAndPostOnly,]
+    queryset = Attachment.objects.all()
+    serializer_class = AttachmentSerializers
+
