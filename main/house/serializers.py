edb73e6510f682142d8310fe2e9e7b73eb331eae
hamzakhalem <hamzakhalem97@gmail.com>
2024-09-12 05:47:50 +0100

    task app

--- main/house/serializers.py
+++ main/house/serializers.py
@@ -4,8 +4,9 @@ class HouseSerializers(serializers.ModelSerializer):
     members_count = serializers.IntegerField(read_only=True)
     members = serializers.HyperlinkedRelatedField(read_only=True, many=True, view_name = 'profile-detail')
     manager = serializers.HyperlinkedRelatedField(read_only=True, many=False, view_name = 'profile-detail')
+    task_lists = serializers.HyperlinkedRelatedField(read_only=True, many=False, view_name = 'tasklist-detail', source="lists")
     class Meta:
         model= House
         fields =['url', 'id', 'user', 'created_on', 'manager', 'description', 'points', 'members'
-                 'completed_tasks_count', 'notcompleted_tasks_count' ]
+                 'completed_tasks_count', 'notcompleted_tasks_count', 'task_lists' ]
         read_only_fields = ['points', 'completed_tasks_count', 'notcompleted_tasks_count']
--- main/task/serializers.py
+++ main/task/serializers.py
@@ -5,9 +5,10 @@ from house.models import House
 class TaskListSerializers(serializers.ModelSerializer):
     house = serializers.HyperlinkedRelatedField(queryset=House.objects.all(), many= False, view_name = 'house-detail')
     created_by = serializers.HyperlinkedRelatedField(read_only=True, many=False, view_name = 'profile-detail')
+    tasks = serializers.HyperlinkedRelatedField(read_only=True, many=True, view_name = 'task-detail')
     class Meta:
         model= TaskList
-        fields =['url', 'id', 'name', 'created_on', 'created_by', 'house', 'description', 'status'  ]
+        fields =['url', 'id', 'name', 'created_on', 'created_by', 'house', 'description', 'status', 'tasks'  ]
         read_only_fields = ['created_on', 'status', 'notcompleted_tasks_count']
 
 
@@ -16,10 +17,13 @@ class TaskSerializers(serializers.ModelSerializer):
     created_by = serializers.HyperlinkedRelatedField(read_only=True, many=False, view_name = 'profile-detail')
     completed_by = serializers.HyperlinkedRelatedField(read_only=True, many=False, view_name = 'profile-detail')
     task_list = serializers.HyperlinkedRelatedField(queryset= TaskList.objects.all(), many=False, view_name = 'tasklist-detail')
+    attachments = serializers.HyperlinkedRelatedField(read_only=True, many=True, view_name = 'attachment-detail')
+
+
     class Meta:
         model= Task
         fields =['url', 'id', 'name', 'created_on', 'completed_on', 'created_by', 'completed_by',
-                 'description', 'status', 'task_lis'  ]
+                 'description', 'status', 'task_lis', 'attachments'  ]
         read_only_fields = ['created_on', 'completed_on', 'created_by', 'completed_by',
                             'status', 'notcompleted_tasks_count']
     
